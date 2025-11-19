2f5f6e97309cff76edad03a92996f86efb04c213
ikaru19 <syafrizalizal17@gmail.com>
2025-11-19 22:43:40 +0700

    Initial Commit

---
+++ GridNation/GridNationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridNationApp.swift
+//  GridNation
+//
+//  Created by Muhammad Syafrizal on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GridNationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
